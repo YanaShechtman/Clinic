812750c1d28cf697fa652a9a59eb01a82c998ef7
YanaShechtman <yana_6@walla.com>
2020-04-06 00:14:15 +0300

    Create RabbitMq Device

---
+++ Clinic/RabbitDevice/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Configuration;
+
+namespace RabbitDevice
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var queueManager = (ConfigurationManager.AppSettings["QueueManagerHostName"]);
+            var rabbitSender = new RabbitSender(queueManager);
+            rabbitSender.SendMessage("hello","hello world");
+            var rabbitReciever = new RabbitReciever(queueManager,"hello");
+        }
+    }
+}
---
+++ Clinic/RabbitDevice/RabbitReciever.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+
+namespace RabbitDevice
+{
+    public class RabbitReceiver : IDisposable
+    {
+        public string HostName { get; set; }
+        private IConnection _connection;
+        private IModel _channel;
+        public string QueueName { get; set; }
+        
+        public RabbitReceiver(string hostName, string queueName)
+        {
+            HostName = hostName;
+            QueueName = queueName;
+            var factory = new ConnectionFactory() { HostName = HostName };
+            _connection = factory.CreateConnection();
+            _channel = _connection.CreateModel();
+            _channel.QueueDeclare(queue: QueueName,
+                durable: false,
+                exclusive: false,
+                autoDelete: false,
+                arguments: null);
+            var consumer = new EventingBasicConsumer(_channel);
+            consumer.Received += ConsumerOnReceived;
+            _channel.BasicConsume(queue: "hello",
+                autoAck: true,
+                consumer: consumer);
+        }
+
+        private void ConsumerOnReceived(object sender, BasicDeliverEventArgs e)
+        {
+            var body = e.Body;
+            var message = Encoding.UTF8.GetString(body);
+        }
+
+        public void Dispose()
+        {
+            _connection?.Dispose();
+            _channel?.Dispose();
+        }
+    }
+}
